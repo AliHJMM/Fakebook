13de04f075904ed3f2942e3783bc5f501aeee104
AliHJMM <AliHasanJMM@gmail.com>
2025-09-29 21:16:14 +0300

    d

--- frontend/src/components/auth/login-buttons.tsx
+++ frontend/src/components/auth/login-buttons.tsx
@@ -1,5 +1,5 @@
-import { Button } from "@/components/ui/button"
-import { Github } from "lucide-react"
+// import { Button } from "@/components/ui/button"
+// import { Github } from "lucide-react"
 
 export function LoginButtons() {
   return (
@@ -8,17 +8,16 @@ export function LoginButtons() {
         <div className="absolute inset-0 flex items-center">
           <span className="w-full border-t" />
         </div>
-        <div className="relative flex justify-center text-xs uppercase">
+        {/* <div className="relative flex justify-center text-xs uppercase">
           <span className="bg-background px-2 text-muted-foreground">Or continue with</span>
-        </div>
+        </div> */}
       </div>
-      <div className="grid grid-cols-1 gap-3">
+      {/* <div className="grid grid-cols-1 gap-3">
         <Button variant="outline" className="hover:bg-zinc-900/5">
           <Github className="mr-2 h-4 w-4" />
           Github
         </Button>
-      </div>
+      </div> */}
     </div>
-  )
+  );
 }
-
